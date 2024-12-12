e7de0ef70c608054e50d352d05995226892a01e4
David Rowe <david.rowe@torqit.ca>
2024-12-12 14:40:12 -0400

    Namespace standardization

--- src/CustomPricing.php
+++ src/CustomPricing.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace TorqCustomPricingExtension;
+namespace Torq\Shopware\CustomPricing;
 
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
@@ -9,7 +9,7 @@ use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 
-class TorqCustomPricingExtension extends Plugin
+class CustomPricing extends Plugin
 {
     public function install(InstallContext $installContext): void
     {
