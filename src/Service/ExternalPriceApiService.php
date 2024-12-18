2a3c7796189fe4dba3e1117d085631136124098f
Jeremy Noonan <jeremy.noonan@torqit.ca>
2024-12-18 08:34:47 -0400

    removing bad service declarations

--- src/Service/ExternalPriceApiService.php
+++ src/Service/ExternalPriceApiService.php
@@ -4,18 +4,10 @@ namespace Torq\Shopware\CustomPricing\Service;
 
 class ExternalPriceApiService implements CustomPriceProvider
 {
-    public function __construct(
-        private readonly CascadeApiWrapper $apiWrapper
-    ) {}
+    public function __construct() {}
 
     public function getCustomPrice(string $customerId, string $customerGroupId, array $productIds): ?array
     {
-        return $this->apiWrapper->getPrices(
-            'defaultCompanyId',
-            $customerId,
-            $productIds,
-            'defaultSourceLocation',
-            'defaultSalesLocation'
-        );
+        return [];
     }
 }
