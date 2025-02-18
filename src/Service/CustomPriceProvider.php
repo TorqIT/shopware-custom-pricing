16a0411a60c1784c7b1097ffab7bee1ec3b135ad
Jeremy Noonan <jeremy.noonan@torqit.ca>
2025-02-18 17:36:07 -0500

    fixing structure of product prices in collection

--- src/Service/CustomPriceProvider.php
+++ src/Service/CustomPriceProvider.php
@@ -7,6 +7,7 @@ use Shopware\Commercial\CustomPricing\Entity\CustomPrice\Price\CustomPrice;
 use Shopware\Core\Content\Product\Aggregate\ProductPrice\ProductPriceCollection;
 use Shopware\Core\Content\Product\Aggregate\ProductPrice\ProductPriceEntity;
 use Shopware\Commercial\CustomPricing\Subscriber\ProductSubscriber;
+use Shopware\Core\Framework\DataAbstractionLayer\Pricing\Price;
 use Shopware\Core\Framework\DataAbstractionLayer\Pricing\PriceCollection;
 use Shopware\Core\Framework\Uuid\Uuid;
 
@@ -33,37 +34,41 @@ abstract class CustomPriceProvider
     }
 
     /**
-     * @param CustomPriceCollection|null $customPrice
+     * @param CustomPriceCollection|null $customPriceCollection
      *
      * @return array{price: PriceCollection|null, prices: ProductPriceCollection, cheapestPrice: null}
      */
-    private function createPriceCollections(string $productId, CustomPriceCollection $customPrice): ?array
+    private function createPriceCollections(string $productId, CustomPriceCollection $customPriceCollection): ?array
     {
-        if ($customPrice === null) {
+        if ($customPriceCollection === null || $customPriceCollection->first() === null) {
             return null;
         }
 
+        //this will need refactored if we ever support tiered pricing
         $productPriceCollection = new ProductPriceCollection();
+        $productPrice = new ProductPriceEntity();
+        $productPrice->setId(Uuid::randomHex());
+        $productPrice->setRuleId(ProductSubscriber::CUSTOMER_PRICE_RULE);
+        $productPrice->setQuantityStart(1);
+        $productPrice->setQuantityEnd(null);
+        $productPrice->setProductId($productId);
+
+        $priceCollection = new PriceCollection();
 
         /** @var CustomPrice $price */
-        foreach ($customPrice as $price) {
-            if ($price === null) {
-                continue;
-            }
-
-            $start = $price->getQuantityStart();
-            $end = $price->getQuantityEnd();
-
-            $productPrice = new ProductPriceEntity();
-            $productPrice->setId(Uuid::randomHex());
-            $productPrice->setRuleId(ProductSubscriber::CUSTOMER_PRICE_RULE);
-            $productPrice->setPrice($customPrice);
-            $productPrice->setProductId($productId);
-            $productPrice->setQuantityStart($start);
-            $productPrice->setQuantityEnd($end);
-
-            $productPriceCollection->add($productPrice);
+        foreach ($customPriceCollection as $price) {
+            
+            $newPrice = new Price(
+                $price->getCurrencyId(),
+                $price->getNet(),
+                $price->getGross(),
+                $price->getLinked()
+            );
+            $priceCollection->add($newPrice);
         }
+
+        $productPrice->setPrice($priceCollection);
+        $productPriceCollection->add($productPrice);
 
         $productPriceCollection->sortByQuantity();
 
